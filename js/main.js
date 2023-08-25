3a6336a823ea8c9b05b096253d6116b61a93968b
AhmedElabrass123 <ahmedelabrass99@gmail.com>
2023-08-26 00:08:55 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+
+// var proImg = document.querySelectorAll(".pro-img");
+// var TheCard = document.querySelectorAll(".card-con");
+// TheCard.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     window.location.href = "singleProduct.html";
+//   });
+// });
